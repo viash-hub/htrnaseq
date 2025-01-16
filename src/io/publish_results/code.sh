7a59281f2737d558af3c3acad19d7b805b32fe91
Toni Verbeiren <toni.verbeiren@gmail.com>
2025-01-16 08:51:22 +0100

    Take into account multiplicity

--- src/io/publish_results/code.sh
+++ src/io/publish_results/code.sh
@@ -9,19 +9,34 @@ mkdir -p "$par_output" && echo "$par_output created"
 echo
 echo "Copying files..."
 IFS=";" read -ra star_output <<<$par_star_output
+IFS=";" read -ra nrReadsNrGenesPerChrom <<<$par_nrReadsNrGenesPerChrom
+IFS=";" read -ra star_qc_metrics <<<$par_star_qc_metrics
+IFS=";" read -ra eset <<<$par_eset
+IFS=";" read -ra f_data <<<$par_f_data
+IFS=";" read -ra p_data <<<$par_p_data
 
 for i in "${star_output[@]}"; do
   cp -rL "$i" "$par_output/"
 done
 
-cp -rL "$par_nrReadsNrGenesPerChrom" "$par_output/"
+for i in "${nrReadsNrGenesPerChrom[@]}"; do
+  cp -rL "$i" "$par_output/"
+done
 
-cp -rL "$par_star_qc_metrics" "$par_output/"
+for i in "${star_qc_metrics[@]}"; do
+  cp -rL "$i" "$par_output/"
+done
 
-cp -rL "$par_eset" "$par_output/"
+for i in "${eset[@]}"; do
+  cp -rL "$i" "$par_output/"
+done
 
-cp -rL "$par_f_data" "$par_output/"
+for i in "${f_data[@]}"; do
+  cp -rL "$i" "$par_output/"
+done
 
-cp -rL "$par_p_data" "$par_output/"
+for i in "${p_data[@]}"; do
+  cp -rL "$i" "$par_output/"
+done
 
 cp -rL "$par_html_report" "$par_output/"
