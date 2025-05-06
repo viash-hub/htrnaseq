afdc3f4ed8c09d39a40ba036f794b7061a689bd6
jakubmajercik <jakub.majercik@gmail.com>
2025-05-06 10:14:48 +0200

    keep id in component

--- src/utils/save_params/script.py
+++ src/utils/save_params/script.py
@@ -21,18 +21,9 @@ def decode_params_yaml(encoded_yaml):
     
     return yaml_data
 
-# def replace_id(value, sample_id):
-#     if isinstance(value, str):
-#         return value.replace('$id', sample_id)
-#     elif isinstance(value, list):
-#         return [replace_id(item, sample_id) for item in value]
-#     return value
-
 print(par['params_yaml'])
 
 params = decode_params_yaml(par['params_yaml'])
-# for key, value in params.items():
-#     params[key] = replace_id(value, par["id"])
 
 with open(par["output"], 'w') as f:
     yaml.dump(params, f, default_flow_style=False, Dumper=Dumper)
