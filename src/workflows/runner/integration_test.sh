6f330613bf93baf9f1c09f08b93ec5dac1d823bc
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2025-09-10 10:40:48 +0000

    Add integration test for generating a single eset

--- src/workflows/runner/integration_test.sh
+++ src/workflows/runner/integration_test.sh
@@ -6,19 +6,29 @@ set -eo pipefail
 # ensure that the command below is run from the root of the repository
 cd "$REPO_ROOT"
 
-nextflow \
-  run . \
-  -main-script src/workflows/runner/test.nf \
-  -config ./src/config/labels.config \
-  -entry test_wf \
-  -resume \
-  -profile docker,local
+# nextflow \
+#   run . \
+#   -main-script src/workflows/runner/test.nf \
+#   -config ./src/config/labels.config \
+#   -entry test_wf \
+#   -resume \
+#   -profile docker,local
+
+# nextflow \
+#   run . \
+#   -main-script src/workflows/runner/test.nf \
+#   -config ./src/config/labels.config \
+#   -entry test_wf_with_lanes \
+#   -resume \
+#   -profile docker,local
+
 
 nextflow \
   run . \
   -main-script src/workflows/runner/test.nf \
   -config ./src/config/labels.config \
-  -entry test_wf_with_lanes \
+  -entry test_wf_only_one_eset \
   -resume \
-  -profile docker,local
+  -profile docker,local \
+  --publish_dir foo
 
