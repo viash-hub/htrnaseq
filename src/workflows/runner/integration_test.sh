70a9caa29eb68754cc5880c013be756b3465ca1d
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2025-09-10 10:43:54 +0000

    Revert some changes and fix config

--- src/workflows/runner/integration_test.sh
+++ src/workflows/runner/integration_test.sh
@@ -6,21 +6,21 @@ set -eo pipefail
 # ensure that the command below is run from the root of the repository
 cd "$REPO_ROOT"
 
-# nextflow \
-#   run . \
-#   -main-script src/workflows/runner/test.nf \
-#   -config ./src/config/labels.config \
-#   -entry test_wf \
-#   -resume \
-#   -profile docker,local
+nextflow \
+  run . \
+  -main-script src/workflows/runner/test.nf \
+  -config ./src/config/labels.config \
+  -entry test_wf \
+  -resume \
+  -profile docker,local
 
-# nextflow \
-#   run . \
-#   -main-script src/workflows/runner/test.nf \
-#   -config ./src/config/labels.config \
-#   -entry test_wf_with_lanes \
-#   -resume \
-#   -profile docker,local
+nextflow \
+  run . \
+  -main-script src/workflows/runner/test.nf \
+  -config ./src/config/labels.config \
+  -entry test_wf_with_lanes \
+  -resume \
+  -profile docker,local
 
 
 nextflow \
@@ -29,6 +29,4 @@ nextflow \
   -config ./src/config/labels.config \
   -entry test_wf_only_one_eset \
   -resume \
-  -profile docker,local \
-  --publish_dir foo
-
+  -profile docker,local
