e111b3f761c20ee2c2d86859768622dcb2c59532
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2025-09-10 15:26:21 +0200

    Add integration test for generating a single eset (#73)

--- src/workflows/runner/integration_test.sh
+++ src/workflows/runner/integration_test.sh
@@ -22,3 +22,11 @@ nextflow \
   -resume \
   -profile docker,local
 
+
+nextflow \
+  run . \
+  -main-script src/workflows/runner/test.nf \
+  -config ./src/config/labels.config \
+  -entry test_wf_only_one_eset \
+  -resume \
+  -profile docker,local
