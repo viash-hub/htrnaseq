f2b6017ba970527583a2f466e0c5e1c15dfa93fd
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2025-09-18 13:07:56 +0200

    Copy symbolic links as-is during publishing (#74)
    * Copy symbolic links as-is during publishing
    
    * Add missing --keep-directory-symlink
    
    * Revert some changes
    
    * Revert some more changes
    
    * Rework solution
    
    * Update CHANGELOG
    
    * Update src/io/publish_results/code.sh
    
    Co-authored-by: Jakub Majercik <57993790+jakubmajercik@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jakub Majercik <57993790+jakubmajercik@users.noreply.github.com>

--- src/io/publish_results/code.sh
+++ src/io/publish_results/code.sh
@@ -7,87 +7,60 @@ echo "Publishing results to multiple output directories"
 # Create output directories for multiple files
 echo "Creating output directories..."
 
-path_pars=(
-  par_star_output_dir
-  par_nrReadsNrGenesPerChrom_dir
-  par_star_qc_metrics_dir
-  par_eset_dir
-  par_f_data_dir
-  par_p_data_dir
-  par_html_report_output
-  par_run_params_output
+declare -A path_pars_dirs=(
+  ["par_star_output_dir"]="par_star_output"
+  ["par_nrReadsNrGenesPerChrom_dir"]="par_nrReadsNrGenesPerChrom"
+  ["par_star_qc_metrics_dir"]="par_star_qc_metrics"
+  ["par_eset_dir"]="par_eset"
+  ["par_f_data_dir"]="par_f_data"
+  ["par_p_data_dir"]="par_p_data"
 )
 
-for par in ${path_pars[@]}; do
+declare -A path_pars_files=(
+  ["par_html_report_output"]="par_html_report"
+  ["par_run_params_output"]="par_run_params"
+)
+
+echo "Canonicalizing output paths."
+all_path_args=( "${!path_pars_files[@]}" "${!path_pars_dirs[@]}" )
+for par in ${all_path_args[@]}; do
     curr_val="${!par}"
+    printf "\t%s\n" "Canonicalizing path '$curr_val'"
     new_value=$(realpath --canonicalize-missing "$curr_val")
+    printf "\t%s\n" "New output path for '$par': '$new_value'"
     declare -g "$par=$new_value"
 done
 
-mkdir -p "$par_star_output_dir" && echo "$par_star_output_dir created"
-mkdir -p "$par_nrReadsNrGenesPerChrom_dir" && echo "$par_nrReadsNrGenesPerChrom_dir created"  
-mkdir -p "$par_star_qc_metrics_dir" && echo "$par_star_qc_metrics_dir created"
-mkdir -p "$par_eset_dir" && echo "$par_eset_dir created"
-mkdir -p "$par_f_data_dir" && echo "$par_f_data_dir created"
-mkdir -p "$par_p_data_dir" && echo "$par_p_data_dir created"
-
-echo
-echo "Copying STAR output files..."
-IFS=";" read -ra star_output <<<$par_star_output
-for i in "${star_output[@]}"; do
-  echo "Copying $i to $par_star_output_dir/"
-  cp -rL "$i" "$par_star_output_dir/"
+echo "Creating output directories"
+for par in ${!path_pars_dirs[@]}; do
+    curr_val="${!par}"
+    mkdir -p "$curr_val" && printf "\t%s\n" "$curr_val created."
+done
+for par in ${!path_pars_files[@]}; do
+    curr_val="${!par}"
+    file_dir=$(dirname "$curr_val")
+    mkdir -p "$file_dir" && printf "\t%s\n" "$file_dir created."
 done
 
-echo
-echo "Copying nrReadsNrGenesPerChrom files..."
-IFS=";" read -ra nrReadsNrGenesPerChrom <<<$par_nrReadsNrGenesPerChrom
-for i in "${nrReadsNrGenesPerChrom[@]}"; do
-  echo "Copying $i to $par_nrReadsNrGenesPerChrom_dir/"
-  cp -rL "$i" "$par_nrReadsNrGenesPerChrom_dir/"
+echo "Copying output for inputs that are directories"
+for par in ${!path_pars_dirs[@]}; do
+  printf "\t%s\n" "Copying output for '$par'"
+  output_val="${!par}"
+  input_par_name="${path_pars_dirs[${par}]}"
+  input_val="${!input_par_name}"
+  IFS=";" read -ra output_files_list <<<$input_val
+  for i in "${output_files_list[@]}"; do 
+      printf "\t%s\n" "Copying '$i' to '$output_val'" 
+      cp -a --keep-directory-symlink "$i" "$output_val/"
+  done
 done
 
-echo
-echo "Copying STAR QC metrics files..."
-IFS=";" read -ra star_qc_metrics <<<$par_star_qc_metrics
-for i in "${star_qc_metrics[@]}"; do
-  echo "Copying $i to $par_star_qc_metrics_dir/"
-  cp -rL "$i" "$par_star_qc_metrics_dir/"
-done
-
-echo
-echo "Copying eset files..."
-IFS=";" read -ra eset <<<$par_eset
-for i in "${eset[@]}"; do
-  echo "Copying $i to $par_eset_dir/"
-  cp -rL "$i" "$par_eset_dir/"
-done
-
-echo
-echo "Copying f_data files..."
-IFS=";" read -ra f_data <<<$par_f_data
-for i in "${f_data[@]}"; do
-  echo "Copying $i to $par_f_data_dir/"
-  cp -rL "$i" "$par_f_data_dir/"
-done
-
-echo
-echo "Copying p_data files..."
-IFS=";" read -ra p_data <<<$par_p_data
-for i in "${p_data[@]}"; do
-  echo "Copying $i to $par_p_data_dir/"
-  cp -rL "$i" "$par_p_data_dir/"
-done
-
-echo
 echo "Copying single files directly..."
-mkdir -p $(dirname "$par_html_report_output")
-echo "Copying $par_html_report to $par_html_report_output"
-cp -L "$par_html_report" "$par_html_report_output"
+for par in ${!path_pars_files[@]}; do
+  output_val="${!par}"
+  input_par_name="${path_pars_files[${par}]}"
+  input_val="${!input_par_name}"
+  cp -a --keep-directory-symlink "$input_val" "$output_val"
+done
 
-echo "Copying $par_run_params to $par_run_params_output"
-mkdir -p $(dirname "$par_run_params_output")
-cp -L "$par_run_params" "$par_run_params_output"
-
-echo
 echo "Publishing completed successfully!"
